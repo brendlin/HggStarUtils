f8bf6cb391b0ba669f2f6fb834a8c8961ae5326f
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2019-08-16 22:26:21 +0200

    Extend cutflow to cover data

--- macros/cutflow.py
+++ macros/cutflow.py
@@ -6,6 +6,18 @@ import PyAnalysisPlotting as anaplot
 import PlotFunctions as plotfunc
 import re
 
+def GetDataCutflowHistograms(t_file) :
+    import re
+    hists = []
+    for i in t_file.GetListOfKeys() :
+        # get _weighted
+        if not re.match('CutFlow_Run.*',i.GetName()) :
+            continue
+        if re.match('CutFlow_.*_.*',i.GetName()) :
+            continue
+        hists.append(i.ReadObj())
+    return hists
+
 def GetWeightedCutflowHistogram(t_file) :
     import re
     for i in t_file.GetListOfKeys() :
@@ -45,8 +57,7 @@ def main(options,args) :
     scales_b = anaplot.GetScales(files_b,trees_b,keys_b,options)
     scales_s = anaplot.GetScales(files_s,trees_s,keys_s,options)
 
-    class tmp_options :
-        mergesamples = {'All Higgs':'%gamstargam%'}
+    hist = None
 
     if options.signal :
 
@@ -73,24 +84,56 @@ def main(options,args) :
             # XS Br Feff from one of the branches
             sig_hists[-1].Scale(GetCrossSectionBRfilterEff(trees_s[k]))
 
+        class tmp_options :
+            mergesamples = {'All Higgs':'%gamstargam%'}
+
         sig_hists = anaplot.MergeSamples(sig_hists,tmp_options)
         print ' ^ ignore this number above ^'
 
-        h = sig_hists[0]
+        hist = sig_hists[0]
 
-        text = ''
-        text += '%s & %s & %s & %s \\\\\n'%('Cut'.ljust(40),'Surviving events'.ljust(17),'Cut efficiency'.ljust(16),'Total efficiency'.ljust(16))
-        for bin in range(3,h.GetNbinsX()+1) :
-            text += ('%s:'%(h.GetXaxis().GetBinLabel(bin))).ljust(40)
-            text += ' & '
-            text += ('%2.2f \\pm %2.2f'%(h.GetBinContent(bin),h.GetBinError(bin))).rjust(17)
-            text += ' & '
-            text += ('%2.1f'%(100*h.GetBinContent(bin)/float(h.GetBinContent(max(bin-1,3))))).rjust(16)
-            text += ' & '
-            text += ('%2.1f'%(100*h.GetBinContent(bin)/float(h.GetBinContent(3)))).rjust(16)
-            text += ' \\\\\n'
+    if options.data :
 
-        print text
+        data_hists = []
+
+        print 'Including the following histograms:'
+
+        for k in sorted(keys_d) :
+
+            for h in GetDataCutflowHistograms(files_d[k]) :
+                data_hists.append(h)
+
+                tags = ['data15','data16','data17','data18']
+                print ' -',''.join(list(a if a in k else '' for a in tags)),data_hists[-1].GetName()
+                data_hists[-1].SetTitle(k+data_hists[-1].GetTitle())
+
+        class tmp_options :
+            mergesamples = {'Data':'data%'}
+
+        data_hist = anaplot.MergeSamples(data_hists,tmp_options,requireFullyMerged=True)[0]
+        print ' ^ ignore this number above ^'
+
+        hist = data_hist
+
+    if not hist :
+        return
+
+    text = ''
+    text += '%s & %s & %s & %s \\\\\n'%('Cut'.ljust(40),'Surviving events'.ljust(17),'Cut efficiency'.ljust(16),'Total efficiency'.ljust(16))
+    for bin in range(3,hist.GetNbinsX()+1) :
+        text += ('%s:'%(hist.GetXaxis().GetBinLabel(bin))).ljust(40)
+        text += ' & '
+        if options.signal :
+            text += ('%2.2f \\pm %2.2f'%(hist.GetBinContent(bin),hist.GetBinError(bin))).rjust(17)
+        else :
+            text += ('%d'%(hist.GetBinContent(bin))).rjust(17)
+        text += ' & '
+        text += ('%2.1f'%(100*hist.GetBinContent(bin)/float(hist.GetBinContent(max(bin-1,3))))).rjust(16)
+        text += ' & '
+        text += ('%2.1f'%(100*hist.GetBinContent(bin)/float(hist.GetBinContent(3)))).rjust(16)
+        text += ' \\\\\n'
+
+    print text
 
     print 'done.'
     return
