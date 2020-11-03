9a12d524c971d494ba21832f5eb9e9db2a61575a
brendlin <kurt.brendlinger@cern.ch>
2020-11-03 15:04:57 +0100

    Update sideband plots to include more options, and unblinded results

--- data/plottrees_SidebandFits.py
+++ data/plottrees_SidebandFits.py
@@ -7,6 +7,7 @@ from HggStarHelpers import ChannelEnum,CategoryEnum
 import StudyConfSnippets
 from HggStarHelpers import StandardSampleMerging as mergesamples
 from HggStarHelpers import StandardHistFormat as histformat
+from array import array
 
 import ROOT
 ROOT.gROOT.LoadMacro('dscb.C')
@@ -31,15 +32,19 @@ higgsSF = 100
 theyear = YEAR.y2015161718
 doMesonCuts = True
 doDetailedVariables = False
+#
 mll_range = [110,160]
 doAddSignal = True
+nBins = 25 # int((mll_range[1]-mll_range[0]))
+doPull = False
+doUnblind = True
 ##
 # End configuration.
 ##
 
 fb = GetFbForMCNormalization(theyear)
 
-histformat['HGamEventInfoAuxDyn.m_lly/1000.'] = [int(mll_range[1]-mll_range[0]),
+histformat['HGamEventInfoAuxDyn.m_lly/1000.'] = [nBins,
                                                  mll_range[0],mll_range[1],'m_{ll#gamma} [GeV]']
 
 leptonObj = {ChannelEnum.DIMUON: 'Muons',
@@ -79,6 +84,8 @@ if region in [REGION.CR1, REGION.CR2, REGION.SR, REGION.SR_VBF] :
 blindcut = [
     '(HGamEventInfoAuxDyn.m_lly < 120000 || 130000 < HGamEventInfoAuxDyn.m_lly)',
     ]
+if doUnblind :
+    blindcut = []
 
 ##
 ## Standard Variables
@@ -109,8 +116,13 @@ def afterburner(can) :
 
     # print list(plotfunc.GetTopPad(can).GetListOfPrimitives())
     hist = plotfunc.GetTopPad(can).GetPrimitive('pad_top_data')
-    ratioplot = hist.Clone()
-    ratioplot.GetYaxis().SetTitle('pull')
+
+    ratioplot = ROOT.TGraphAsymmErrors()
+    ratioplot.SetMarkerSize(1.0)
+    ratioplot.SetLineWidth(2)
+    ratioplot.GetYaxis().SetTitle('Residual')
+    if doPull :
+        ratioplot.GetYaxis().SetTitle('Pull')
     ratioplot.GetXaxis().SetTitle('m_{ll#gamma} [GeV]')
 
     print 'Expecting to open a file bkgParameters.txt'
@@ -137,6 +149,7 @@ def afterburner(can) :
         }.get(function)
 
     f_bkg = ROOT.TF1(function,expr,mll_range[0],mll_range[1])
+    f_bkg.SetName('bkg_function')
     f_bkg.SetTitle('Bkg (%s)'%function)
 
     translation = {
@@ -179,27 +192,12 @@ def afterburner(can) :
         f_bkg.SetParameter(1,par)
 
     f_bkg.SetParameter(0,1)
-    integral = f_bkg.Integral(mll_range[0],mll_range[1])
-    f_bkg.SetParameter(0,parameters['nbkg_%s'%(cstr)]/float(integral))
+    integral = f_bkg.Integral(110,160) # The range for which it was performed
+    bin_width = (mll_range[1]-mll_range[0])/float(nBins)
+    f_bkg.SetParameter(0,bin_width*parameters['nbkg_%s'%(cstr)]/float(integral))
     f_bkg.SetLineColor(ROOT.kBlue)
     f_bkg.SetLineWidth(2)
     plotfunc.AddHistogram(can,f_bkg,'l')
-
-    # Pull!
-    for i in range(ratioplot.GetNbinsX()+2) :
-        bc1 = hist.GetBinContent(i)
-        bc2 = f_bkg.Eval(hist.GetBinCenter(i)) #ref_hist.GetBinContent(i)
-        be1 = hist    .GetBinErrorLow(i) if (bc1 > bc2) else hist    .GetBinErrorUp(i)
-        be2 = 0 # ref_hist.GetBinErrorLow(i) if (bc2 > bc1) else ref_hist.GetBinErrorUp(i)
-
-        if (be1**2 + be2**2) :
-            ratioplot.SetBinContent(i,(bc1-bc2)/math.sqrt(be1**2+be2**2))
-        ratioplot.SetBinError(i,1)
-
-        if 120 < hist.GetBinCenter(i) and hist.GetBinCenter(i) < 130 :
-            ratioplot.SetBinContent(i,-99)
-
-    plotfunc.AddHistogram(can.GetPrimitive('pad_bot'),ratioplot,drawopt='pE1')
 
     #
     # Add signal and Hyy:
@@ -210,8 +208,13 @@ def afterburner(can) :
         i = i.replace('\n','')
         parameters[i.split()[0]] = float(i.split()[1])
 
+    h_hyyOnly = None
+    h_sigOnly = None
+    nhyy = None
+
     if doAddSignal :
         f_sig = ROOT.TF1('H#rightarrow#gamma*#gamma parameterization',ROOT.dscb,80,180,7)
+        f_sig.SetName('signal_function')
 
         # The numbering of resonance_paramList is offset by 1.
         c = category - 1
@@ -228,44 +231,119 @@ def afterburner(can) :
         integral_sig = parameters['sigYield_SM_m125000_c%d'%(c)]
         f_sig.SetParameter(0,integral_sig*hist.GetBinWidth(1)/float(integral)) # bin width!!
 
-        h_hyy = None
+        # H->yy Background
         nhyy = parameters.get('hyy_%s'%(cstr),0)
-        if nhyy :
-            h_hyy = ROOT.TH1F('h_hyy','Bkg^{ }+^{ }H#rightarrow#gamma#gamma',40,120,130)
-            sig_int = f_sig.Integral(105,160)
-            for i in range(h_hyy.GetNbinsX()) :
-                bcenter = h_hyy.GetBinCenter(i+1)
-                bcontent = f_sig.Eval(bcenter)*nhyy/float(sig_int) + f_bkg.Eval(bcenter)
-                h_hyy.SetBinContent(i+1,bcontent)
 
-            h_hyy.SetLineColor(ROOT.kGreen+1)
-            h_hyy.SetLineStyle(7)
-            h_hyy.SetLineWidth(2)
-            plotfunc.AddHistogram(can,h_hyy,'l')
+        # mu
+        mu = parameters.get('mu',1.0)
+        print 'mu:',mu
+
+        h_hyy = ROOT.TH1F('h_hyy','Bkg^{ }+^{ }H#rightarrow#gamma#gamma',40,120,130)
+        h_hyyOnly = ROOT.TH1F('h_hyyOnly','H#rightarrow#gamma#gamma ONLY',40,120,130)
+        for i in range(h_hyy.GetNbinsX()) :
+            bcenter = h_hyy.GetBinCenter(i+1)
+            bcontentHyyOnly = f_sig.Eval(bcenter)*nhyy/float(integral_sig)
+            bcontent =  bcontentHyyOnly + f_bkg.Eval(bcenter)
+            h_hyy.SetBinContent(i+1,bcontent)
+            h_hyyOnly.SetBinContent(i+1,bcontentHyyOnly)
+
+        h_hyy.SetLineColor(ROOT.kGreen+1)
+        h_hyy.SetLineStyle(7)
+        h_hyy.SetLineWidth(2)
+        plotfunc.AddHistogram(can,h_hyy,'l')
 
         integral_hyy = 1
-        h_sig = ROOT.TH1F('h_sig','Sig^{ }+^{ }Bkg',40,120,130)
+        h_sig = ROOT.TH1F('h_sig','Sig#times%.2f^{ }+^{ }Bkg'%(mu),40,120,130)
+        h_sigOnly = ROOT.TH1F('h_sigOnly','Sig#times%.2f^{ }+^{ }Hyy ONLY'%(mu),40,120,130)
         if nhyy :
-            h_sig.SetTitle('Sig^{ }+^{ }Bkg^{ }+^{ }H#rightarrow#gamma#gamma')
+            h_sig.SetTitle('Sig^{ }#times%.2f+^{ }Bkg^{ }+^{ }H#rightarrow#gamma#gamma'%(mu))
         for i in range(h_sig.GetNbinsX()) :
             bcenter = h_sig.GetBinCenter(i+1)
-            h_sig.SetBinContent(i+1,f_sig.Eval(bcenter) + f_bkg.Eval(bcenter))
+            sigPlusHyy = (integral_sig*mu + nhyy)*f_sig.Eval(bcenter)/float(integral_sig)
+            h_sig.SetBinContent(i+1,sigPlusHyy + f_bkg.Eval(bcenter))
+            h_sigOnly.SetBinContent(i+1,sigPlusHyy)
+
         h_sig.SetLineColor(ROOT.kRed)
         plotfunc.AddHistogram(can,h_sig,'l')
+
+    # Pull!
+    #for i in range(ratioplot.GetNbinsX()+2) :
+    for i in range(1,hist.GetNbinsX()+1) :
+        bc1 = hist.GetBinContent(i)
+
+        if doAddSignal and doPull :
+            #Pull WRT signal + hyy + background
+            bcenter = hist.GetBinCenter(i)
+            bc2 = f_bkg.Eval(bcenter) + (integral_sig + nhyy)*f_sig.Eval(bcenter)/float(integral_sig)
+        elif doAddSignal and (not doPull) :
+            #ratio WRT background-only
+            bcenter = hist.GetBinCenter(i)
+            bc2 = f_bkg.Eval(bcenter)
+
+        else :
+            # Pull WRT background-only
+            bc2 = f_bkg.Eval(hist.GetBinCenter(i)) #ref_hist.GetBinContent(i)
+
+        be1 = hist    .GetBinErrorLow(i) if (bc1 > bc2) else hist    .GetBinErrorUp(i)
+        be2 = 0 # ref_hist.GetBinErrorLow(i) if (bc2 > bc1) else ref_hist.GetBinErrorUp(i)
+
+        if (be1**2 + be2**2) :
+            ratioplot.SetPoint(i-1,
+                               mll_range[0] + (i-1+0.5)*(mll_range[1] - mll_range[0])/float(nBins),
+                               (bc1-bc2))
+            #ratioplot.SetBinContent(i,(bc1-bc2))
+            if doPull :
+                ratioplot.SetPoint(i-1,
+                                   mll_range[0] + (i-1+0.5)*(mll_range[1]-mll_range[0])/float(nBins),
+                                   (bc1-bc2)/math.sqrt(be1**2+be2**2))
+
+        #ratioplot.SetBinError(i,be1)
+        ratioplot.SetPointError(i-1,(0.5)*(mll_range[1] - mll_range[0])/float(nBins),
+                                (0.5)*(mll_range[1] - mll_range[0])/float(nBins),
+                                hist.GetBinErrorLow(i),
+                                hist.GetBinErrorUp(i))
+        if doPull :
+            ratioplot.SetPointError(i-1,
+                                    (0.5)*(mll_range[1] - mll_range[0])/float(nBins),
+                                    (0.5)*(mll_range[1] - mll_range[0])/float(nBins),
+                                    1,1)
+
+        if blindcut and 120 < hist.GetBinCenter(i) and hist.GetBinCenter(i) < 130 :
+            ratioplot.SetPoint(i-1,
+                               mll_range[0] + (i-1+0.5)*(mll_range[1]-mll_range[0])/float(nBins),
+                               -99)
+
+    plotfunc.AddHistogram(can.GetPrimitive('pad_bot'),ratioplot,drawopt='pE1')
+    if (not doPull) and nhyy :
+        h_hyyOnly.SetLineColor(ROOT.kGreen+1)
+        plotfunc.AddHistogram(can.GetPrimitive('pad_bot'),h_hyyOnly,drawopt='l')
+    if (not doPull) and h_sigOnly :
+        h_sigOnly.SetLineColor(ROOT.kRed)
+        plotfunc.AddHistogram(can.GetPrimitive('pad_bot'),h_sigOnly,drawopt='l')
+        plotfunc.AddHistogram(can.GetPrimitive('pad_bot'),ratioplot,drawopt='pE1')
 
     ranges = plotfunc.AutoFixYaxis(plotfunc.GetTopPad(can))
     plotfunc.SetYaxisRanges(plotfunc.GetTopPad(can),0.001,ranges[1])
 
     # Set ratio range; add dotted line at 1
     if plotfunc.GetBotPad(can) :
-        taxisfunc.SetYaxisRanges(plotfunc.GetBotPad(can),-3.999,3.999)
+
+        # Set the bottom plot ranges to something symmetric, based on the upper limit.
+        ranges = plotfunc.AutoFixYaxis(plotfunc.GetBotPad(can))
+        plotfunc.SetYaxisRanges(plotfunc.GetBotPad(can),-1.5*ranges[1],1.5*ranges[1])
+
+        taxisfunc.SetXaxisRanges(plotfunc.GetBotPad(can),mll_range[0],mll_range[1])
+        if doPull :
+            taxisfunc.SetYaxisRanges(plotfunc.GetBotPad(can),-3.999,3.999)
+
         xmin,xmax = None,None
         for i in plotfunc.GetBotPad(can).GetListOfPrimitives() :
             if issubclass(type(i),ROOT.TH1) :
                 xmin = i.GetXaxis().GetBinLowEdge(1)
                 xmax = i.GetXaxis().GetBinLowEdge(i.GetNbinsX()+1)
-        if xmin != None :
-            line = ROOT.TLine(xmin,0,xmax,0)
+
+        if True :
+            line = ROOT.TLine(mll_range[0],0,mll_range[1],0)
             line.SetLineStyle(2)
             plotfunc.GetBotPad(can).cd()
             line.Draw()
@@ -279,5 +357,22 @@ def afterburner(can) :
         plotfunc.AddHistogram(can,h_hyy,'l')
     plotfunc.AddHistogram(can,hist)
     plotfunc.FormatCanvasAxes(can)
+
+    # return
+
+    f = ROOT.TFile('Results_%s.root'%(category),'RECREATE')
+    for i in list(plotfunc.GetTopPad(can).GetListOfPrimitives() +
+                  plotfunc.GetBotPad(can).GetListOfPrimitives() ) :
+
+        if issubclass(type(i),ROOT.TLegend) :
+            continue
+        elif issubclass(type(i),ROOT.TF1) :
+            tmp = i.GetHistogram()
+            tmp.SetName(i.GetName())
+            tmp.Write()
+        else :
+            i.Write()
+
+    f.Close()
 
     return
